5ea7438c4392a3fd1f7d93018c4192cde7793b4f
binur-torrent <binur20072305@gmail.com>
2026-03-26 16:01:52 +0300

    Popup frontend ready

--- ProjectX/MainView/FolderView/PopoverFolderView.swift
+++ ProjectX/MainView/FolderView/PopoverFolderView.swift
@@ -21,55 +21,27 @@ struct PopoverFolderView: View {
     @State private var showViewer = false
     @State private var selectedFileURLs: [URL] = []
     
+    @FocusState private var isTyping: Bool
+    
     var body: some View {
         ZStack{
             VStack{
-                TextField("Write your name", text: $name)
-                    .font(.title)
-                    .padding()
-                Button {
-                    showFileImporter = true
-                } label: {
-                    Label("Choose file to import", systemImage: "doc.circle")
-                        .font(Font.title.bold())
-                        .frame(width: 300, height: 100)
-                        .foregroundStyle(.white)
-                        .background(Color.black)
-                        .cornerRadius(20)
+                ZStack(alignment: .leading){
+                    TextField("", text: $name)
+                        .padding(.leading)
+                        .frame(height: 60).focused($isTyping)
+                        .background(isTyping ? Color("bottonPurple") : Color("secondaryGray"),  in: RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2))
+                        .padding()
+                        .autocorrectionDisabled()
+                    Text("Frist Name")
+                        .padding(.horizontal, 5)
+                        .background(.white.opacity(isTyping || !name.isEmpty ? 1 : 0))
+                        .foregroundStyle(isTyping || !name.isEmpty ? Color("bottonPurple") : Color("secondaryGray"))
+                        .padding(.leading, 30).offset(y: isTyping || !name.isEmpty ? -30 : 0)
                 }
-                .fileImporter(isPresented: $showFileImporter,
-                              allowedContentTypes: [.data],
-                              allowsMultipleSelection: true
-                ) { result in
-                    switch result {
-                    case .success(let files):
-                        if !files.isEmpty {
-                            selectedFileURLs = files
-                            showViewer = true
-                            
-                            
-                            // in case i want to save reopen the files that user selected even after app closed
-                                    /*files.forEach { file in
-                                        // Get access to the file
-                                        let gotAccess = file.startAccessingSecurityScopedResource()
-                                        if !gotAccess { return }
-                                        
-                                        // Stop accessing when scope ends (guarantee)
-                                        defer{
-                                            file.stopAccessingSecurityScopedResource()
-                                        }
-                                        do{
-                                            let bookmarkData = try file.bookmarkData(options: .minimalBookmark)
-                                        }
-                                        catch {
-                                            print("Failed to create bookmark: \(error.localizedDescription)")
-                                        }*/
-                            
-                            
-                        }
-                    case .failure(let error): print("File imprter error: \(error)")
-                    }
-                }
+                .animation(.linear(duration: 0.2), value: isTyping)
+                
+                
                 Button{
                     if !name.trimmingCharacters(in: .whitespaces).isEmpty {
                         onCreate(name)
@@ -77,10 +49,10 @@ struct PopoverFolderView: View {
                         dismiss()
                     }
                 }label:{
-                    Text("DONE")
+                    Text("Create Folder")
                 }
-                .frame(width: 200, height: 100)
-                .font(Font.largeTitle)
+                .frame(height: 80)
+                .background(name.isEmpty ? Color("bottonPurple") : Color("secondaryGray"),  in: RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2))
             }
         }
         
